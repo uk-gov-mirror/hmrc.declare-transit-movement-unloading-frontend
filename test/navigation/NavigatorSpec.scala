40a05f6fc34318829220363464fce0413b5e9658
kenneth.rayner <kenneth.rayner@digital.hmrc.gov.uk>
2020-04-07 10:36:34 +0100

    CTCTRADERS-344-nav: amend nav for seal question pages

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -56,14 +56,14 @@ class NavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with Generato
           }
         }
 
-        "to session expired page when the answer is No" in {
+        "to Are any seals broken page  when the answer is No" in {
 
           forAll(arbitrary[UserAnswers]) {
             answers =>
               val updatedUserAnswers = answers.set(CanSealsBeReadPage, false).success.value
               navigator
                 .nextPage(CanSealsBeReadPage, NormalMode, updatedUserAnswers)
-                .mustBe(routes.SessionExpiredController.onPageLoad())
+                .mustBe(routes.AreAnySealsBrokenController.onPageLoad(updatedUserAnswers.id, NormalMode))
           }
         }
 
@@ -92,7 +92,7 @@ class NavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with Generato
           }
         }
 
-        "to session expired page when the answer is Yes" in {
+        "to unloading summary page when the answer is Yes" in {
 
           forAll(arbitrary[UserAnswers]) {
             answers =>
@@ -100,7 +100,7 @@ class NavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with Generato
 
               navigator
                 .nextPage(AreAnySealsBrokenPage, NormalMode, updatedUserAnswers)
-                .mustBe(routes.SessionExpiredController.onPageLoad())
+                .mustBe(routes.UnloadingSummaryController.onPageLoad(updatedUserAnswers.id))
           }
         }
 
