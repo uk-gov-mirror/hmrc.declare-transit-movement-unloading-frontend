2d7c5886b3a3d299407a4486ad0dff7f5cd6dc65
PSouthern <paulsouthern81@hotmail.co.uk>
2020-04-15 08:31:03 +0100

    Adding id attirubte

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -157,7 +157,8 @@ class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messag
           Action(
             content            = msg"site.edit",
             href               = routes.DateGoodsUnloadedController.onPageLoad(userAnswers.id, CheckMode).url,
-            visuallyHiddenText = Some(msg"site.edit.hidden".withArgs(msg"dateGoodsUnloaded.checkYourAnswersLabel"))
+            visuallyHiddenText = Some(msg"site.edit.hidden".withArgs(msg"dateGoodsUnloaded.checkYourAnswersLabel")),
+            attributes         = Map("id" -> s"""change-date-goods-unloaded""")
           )
         )
       )
