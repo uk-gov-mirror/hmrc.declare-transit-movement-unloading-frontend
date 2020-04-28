f7299c42ac8ec9521c53430a64f2af59624e5c26
PSouthern <paulsouthern81@hotmail.co.uk>
2020-04-28 10:43:57 +0100

    Adding new files

---
+++ migrations/applied_migrations/ConfirmRemoveSeal.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration ConfirmRemoveSeal"
+
+echo "Adding routes to conf/app.routes"
+
+echo "" >> ../conf/app.routes
+echo "GET        /:mrn/confirmRemoveSeal                        controllers.ConfirmRemoveSealController.onPageLoad(mrn: MovementReferenceNumber, mode: Mode = NormalMode)" >> ../conf/app.routes
+echo "POST       /:mrn/confirmRemoveSeal                        controllers.ConfirmRemoveSealController.onSubmit(mrn: MovementReferenceNumber, mode: Mode = NormalMode)" >> ../conf/app.routes
+
+echo "GET        /:mrn/changeConfirmRemoveSeal                  controllers.ConfirmRemoveSealController.onPageLoad(mrn: MovementReferenceNumber, mode: Mode = CheckMode)" >> ../conf/app.routes
+echo "POST       /:mrn/changeConfirmRemoveSeal                  controllers.ConfirmRemoveSealController.onSubmit(mrn: MovementReferenceNumber, mode: Mode = CheckMode)" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "confirmRemoveSeal.title = confirmRemoveSeal" >> ../conf/messages.en
+echo "confirmRemoveSeal.heading = confirmRemoveSeal" >> ../conf/messages.en
+echo "confirmRemoveSeal.checkYourAnswersLabel = confirmRemoveSeal" >> ../conf/messages.en
+echo "confirmRemoveSeal.error.required = Select yes if confirmRemoveSeal" >> ../conf/messages.en
+
+echo "Adding to UserAnswersEntryGenerators"
+awk '/self: Generators =>/ {\
+    print;\
+    print "";\
+    print "  implicit lazy val arbitraryConfirmRemoveSealUserAnswersEntry: Arbitrary[(ConfirmRemoveSealPage.type, JsValue)] =";\
+    print "    Arbitrary {";\
+    print "      for {";\
+    print "        page  <- arbitrary[ConfirmRemoveSealPage.type]";\
+    print "        value <- arbitrary[Boolean].map(Json.toJson(_))";\
+    print "      } yield (page, value)";\
+    print "    }";\
+    next }1' ../test/generators/UserAnswersEntryGenerators.scala > tmp && mv tmp ../test/generators/UserAnswersEntryGenerators.scala
+
+echo "Adding to PageGenerators"
+awk '/trait PageGenerators/ {\
+    print;\
+    print "";\
+    print "  implicit lazy val arbitraryConfirmRemoveSealPage: Arbitrary[ConfirmRemoveSealPage.type] =";\
+    print "    Arbitrary(ConfirmRemoveSealPage)";\
+    next }1' ../test/generators/PageGenerators.scala > tmp && mv tmp ../test/generators/PageGenerators.scala
+
+echo "Adding to UserAnswersGenerator"
+awk '/val generators/ {\
+    print;\
+    print "    arbitrary[(ConfirmRemoveSealPage.type, JsValue)] ::";\
+    next }1' ../test/generators/UserAnswersGenerator.scala > tmp && mv tmp ../test/generators/UserAnswersGenerator.scala
+
+echo "Adding helper method to CheckYourAnswersHelper"
+awk '/class CheckYourAnswersHelper/ {\
+     print;\
+     print "";\
+     print "  def confirmRemoveSeal: Option[Row] = userAnswers.get(ConfirmRemoveSealPage) map {";\
+     print "    answer =>";\
+     print "      Row(";\
+     print "        key     = Key(msg\"confirmRemoveSeal.checkYourAnswersLabel\", classes = Seq(\"govuk-!-width-one-half\")),";\
+     print "        value   = Value(yesOrNo(answer)),";\
+     print "        actions = List(";\
+     print "          Action(";\
+     print "            content            = msg\"site.edit\",";\
+     print "            href               = routes.ConfirmRemoveSealController.onPageLoad(mrn, CheckMode).url,";\
+     print "            visuallyHiddenText = Some(msg\"site.edit.hidden\".withArgs(msg\"confirmRemoveSeal.checkYourAnswersLabel\"))";\
+     print "          )";\
+     print "        )";\
+     print "      )";\
+     print "  }";\
+     next }1' ../app/utils/CheckYourAnswersHelper.scala > tmp && mv tmp ../app/utils/CheckYourAnswersHelper.scala
+
+echo "Migration ConfirmRemoveSeal completed"
