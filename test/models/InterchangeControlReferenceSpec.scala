6b67ac377f29e49633ae291a44abec2736914814
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2020-05-12 08:32:42 +0100

    Add InterchangeControlReference

---
+++ test/models/InterchangeControlReferenceSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+import models.messages.{escapeXml, InterchangeControlReference}
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalatest.{FreeSpec, MustMatchers, StreamlinedXmlEquality}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import models.XMLWrites._
+
+import scala.xml.NodeSeq
+
+class InterchangeControlReferenceSpec extends FreeSpec with MustMatchers with ScalaCheckPropertyChecks with StreamlinedXmlEquality {
+
+  "InterchangeControlReference" - {
+    "must convert to xml and convert to correct format" in {
+      forAll(arbitrary[String], arbitrary[Int]) {
+        (date, index) =>
+          val expectedResult: NodeSeq = <IntConRefMES11>{s"UF${escapeXml(date)}$index"}</IntConRefMES11>
+
+          val interchangeControlReference = InterchangeControlReference(date, index)
+          val result                      = interchangeControlReference.toXml
+
+          result mustEqual expectedResult
+      }
+    }
+  }
+}
